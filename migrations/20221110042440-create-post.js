c0b76597867e4765496fe9abd95333f34e20fdca
aniisah <anisaayu735@gmail.com>
2022-11-10 12:11:12 +0700

    dataa

---
+++ migrations/20221110042440-create-post.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+   up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      like: {
+        type: Sequelize.INTEGER
+      },
+      profileId: {
+        type: Sequelize.INTEGER, 
+        references: {
+          model: "Profiles", 
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+   down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Posts');
+  }
+};
