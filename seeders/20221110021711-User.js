66b65cd3e6c492f7925b0503acdecb56543aef8a
aniisah <anisaayu735@gmail.com>
2022-11-10 11:47:21 +0700

    data

---
+++ seeders/20221110021711-User.js
@@ -0,0 +1,18 @@
+'use strict';
+let fs = require('fs');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+   up (queryInterface, Sequelize) {
+    let data = JSON.parse(fs.readFileSync('./data/user.json', 'utf8')).map((el)=>{
+      el.createdAt = new Date()
+      el.updatedAt = new Date()
+      delete el.id 
+      return el
+    })
+      return queryInterface.bulkInsert('Users', data, {});
+  },
+
+   down (queryInterface, Sequelize) {
+      return queryInterface.bulkDelete('Users', null, {});
+  }
+};
